2f1335a5b437d21ff05e83c5f21822df6b4d252f
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-11 18:42:43 -0500

    Initialized tree.py, grabs input, and prints it

---
+++ tree.py
@@ -0,0 +1,16 @@
+"""
+NeuroScouting Interview Tree
+
+Usage: python tree.py [starting value]
+
+Description can be found in readme associated.
+
+author: Chris Lee
+created: 11/11/2013
+"""
+
+
+if __name__ == "__main__":
+	startingValue = raw_input("Input the value for the root node:")
+	print "Entered ", startingValue
+	#Create a binary tree class - Left and Right child nodes

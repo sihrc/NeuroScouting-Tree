3eafdf4c39cf0ee92a644b9b583fc8d791894f03
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-11 22:06:12 -0500

    Finished first implementation, can make printer better but... work on second implementation first

---
+++ tree2.py
@@ -0,0 +1,19 @@
+"""
+NeuroScouting Interview Tree
+
+Usage: python tree2.py [starting value]
+
+Description can be found in readme associated.
+Interpretation 2 - node's sibling means any node on the same depth
+author: Chris Lee
+created: 11/11/2013
+"""
+
+#Because I already used a tree, I don't feel like using a tree again.
+#Even though the tree takes in a general function to build the node.
+#SKELETON TREES
+
+if __name__ == "__main__":
+	startingLine = [1]	
+
+

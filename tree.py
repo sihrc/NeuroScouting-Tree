4dbf76661fc5855b0b596447bd51ede66ed6e56e
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-11 22:06:10 -0500

    Finished first implementation, can make printer better but... work on second implementation first

--- tree.py
+++ tree.py
@@ -4,7 +4,7 @@ NeuroScouting Interview Tree
 Usage: python tree.py [starting value]
 
 Description can be found in readme associated.
-
+Interpretation 1 - node's sibling means a node that shares the same immediate parent
 author: Chris Lee
 created: 11/11/2013
 """
@@ -76,7 +76,6 @@ class BinaryTree:
 """
 Attributes:
 	binaryTree (binaryTree)
-	maxDepth (maximum depth of tree)
 	
 Public Methods:
 	printOut - takes in filepath returns none - outputs the tree printed in the file specified at the filepath
@@ -84,22 +83,36 @@ Public Methods:
 class BinaryTreePrinter:
 	def __init__(self, binaryTree = BinaryTree()):
 		self.binaryTree = binaryTree
-		self.maxDepth = binaryTree.maxDepth
 
 	#writes the tree to file
 	def printOut(self,filepath):
 		nodes = self.getNodes()
+		maxWidth = self.getTreeWidth(nodes)
+		with open(filepath,'wb') as f:
+			for i in range(self.binaryTree.maxDepth):
+				f.write(self.getLine(nodes[i],maxWidth))
+
+	def getLine(self, line, maxWidth):
+		numSpaces = len(line) + 1
+		spacing = maxWidth/numSpaces
+		lineBuilder = ""
+		for value in line:
+			value = str(value)
+			half = len(value)/2
+			lineBuilder += (spacing - half) * " " + value + (len(value) - half) * " "
+		lineBuilder += "\n"
+		return lineBuilder
 
 	#get character width of printed tree
 	def getTreeWidth(self, nodeDict):
-		return sum([len(str(node.value)) for node in nodeDict[self.maxDepth]])
+		return sum([len(str(value)) + 1 for value in nodeDict[self.binaryTree.maxDepth]])
 
 	#gets all nodes in the tree
 	def getNodes(self):
-		return self.privateGetNodes(self.root, dict(), 0)
+		return self.privateGetNodes(self.binaryTree.root, dict(), 0)
 	
 	def privateGetNodes(self,node,nodes,depth):
-		nodes[depth] = nodes.get(depth,[]) + [node]
+		nodes[depth] = nodes.get(depth,[]) + [node.value]
 		nodes = self.privateGetNodes(node.left, nodes, depth + 1) if node.left else nodes
 		nodes = self.privateGetNodes(node.right, nodes, depth + 1) if node.right else nodes
 		return nodes
